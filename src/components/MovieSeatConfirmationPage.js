2ad197c7c5512b9ab7ce3d1445e943a813fdc02e
nylol <37283594+naiyuh95@users.noreply.github.com>
2021-09-13 19:24:59 +0800

    Update MovieSeatConfirmationPage.js

--- src/components/MovieSeatConfirmationPage.js
+++ src/components/MovieSeatConfirmationPage.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import '../styles/Header.css'
-
-
+import { Link } from 'react-router-dom'
+import MovieCard from './MovieCard.js'
+import Seat from './Seat.js'
+import axios from 'axios'
+import { NodeMinusFill, Square } from 'react-bootstrap-icons'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function MovieSeatConfirmationPage() {
